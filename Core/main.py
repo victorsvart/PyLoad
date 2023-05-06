6075c3e2ace27b6dc20611536bea055c636c760f
Victor Moraes <victormoraespt4@gmail.com>
2023-05-06 18:25:50 -0300

    Further Abstraction on Interface

--- Core/main.py
+++ Core/main.py
@@ -7,92 +7,76 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 import customtkinter as ctk
 from Services.app_services import *
 from threading import Thread as th
-from Repositories.app_handler import App
 from Repositories.tableandwidget_handler import TabAndWidgetHandler
 threads = Threads()
 
 services = Services()
 
 
-class MyTabView(TabAndWidgetHandler):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
-
-        UIFont = ctk.CTkFont(family="Segoe UI", weight="bold")
-        self.tabs = self.addTab(2, ["Download", "Playlist"])
+class MyTabView(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Pyload")
+        self.geometry("650x400")
+        self._set_appearance_mode("dark")
+        
 
         self.checkboxValue = ctk.IntVar()
-        self.frameList = self.setFrame(self.tabs, 2, self.tabs)
-        self.setLabel(self.frameList[0], "Insert an YouTube link")
-        self.setEntry(self.frameList[0], "Enter a valid link")
-        self.setCheckBox(self.frameList[0], "Audio Only", self.checkboxValue)
-        self.progressBar = self.setProgressBar(self.frameList[0], "horizontal", "#035bbd", "undeterminate")
-        self.button(self.frameList[0], "#035bbd", UIFont, "Download", command=lambda: [self.progressBar.start(), 
+        self.handler = TabAndWidgetHandler(master=self)
+        self.handler.setTabAndFrame(tabName="Download")
+        self.handler.setLabel(text="Insert an YouTube link")
+        self.handler.setEntry(placeholder_text="Enter a valid link")
+        self.handler.setCheckBox(text="Audio Only", variable=self.checkboxValue)
+        self.handler.setProgressBar(orientation="horizontal", 
+                                   progress_color="#035bbd", 
+                                    mode="undeterminate")
+        self.handler.setButton(hover_color="#035bbd", text="Download", command=lambda: [self.handler.progressBar.start(),
                                                                                        self.handleThread()])
-        self.progressBar.pack(pady=12, padx=10)
+
+        self.handler.setTabAndFrame(tabName="Playlist")
+        self.handler.pack(padx=20, pady=20, fill="both", expand=True)
+        
+        self.handler.pack(padx=20, pady=20, fill="both", expand=True)
 
 
-        # Download Tab
-        # self.frame = ctk.CTkFrame(master=self.tab("Download"))
-        # self.frame.pack(pady=30, padx=60, fill="both", expand=True)
-        # self.label = ctk.CTkLabel(master=self.frame, text="Insert an YouTube link")
-        # self.label.pack(pady=12, padx=10)
+        
+       
+        # self.playlistTab = TabAndWidgetHandler(self, tabName="Playlist")
+        # self.setTabAndFrame()
+        # self.playlistTab.setLabel(text="Insert an YouTube link")
 
-        # self.entry0 = ctk.CTkEntry(master=self.frame,
-        #                       placeholder_text="Enter a valid link")
-        # self.entry0.pack(pady=10, padx=10)
+
+        # UIFont = ctk.CTkFont(family="Segoe UI", weight="bold")
+        # self.tabs = self.addTab(2, ["Download", "Playlist"])
 
         # self.checkboxValue = ctk.IntVar()
-        # self.checkbox = ctk.CTkCheckBox(master=self.frame,
-        #                            text="Audio Only",
-        #                            variable=self.checkboxValue,
-        #                            onvalue=1,
-        #                            offvalue=0)
-        # self.checkbox.pack(pady=12, padx=10)
-
-       
-
-        # self.downloadButton = ctk.CTkButton(
-        #     hover_color="#035bbd",
-        #     font=UIFont,
-        #     master=self.frame,
-        #     text="Download",
-        #     command=lambda: [self.progressBar.start(), self.handleThread()])
-        # self.downloadButton.pack(pady=12, padx=10)
-
-        # self.progressBar = ctk.CTkProgressBar(master=self.frame, orientation="horizontal", progress_color="darkgrey",  mode="undeterminate")
+        # self.frameList = self.setFrame(self.tabs, self.tabs)
+        # self.setLabel(self.frameList[0], "Insert an YouTube link")
+        # self.setEntry(self.frameList[0], "Enter a valid link")
+        # self.setCheckBox(self.frameList[0], "Audio Only", self.checkboxValue)
+        # self.progressBar = self.setProgressBar(self.frameList[0], "horizontal", "#035bbd", "undeterminate")
+        # self.button(self.frameList[0], "#035bbd", UIFont, "Download", command=lambda: [self.progressBar.start(), 
+        #                                                                                self.handleThread()])
         # self.progressBar.pack(pady=12, padx=10)
-
-
-        # # Playlist Tab
-        # self.frame2 = ctk.CTkFrame(master=self.tab("Playlist"))
-        # self.frame2.pack(pady=30, padx=60, fill="both", expand=True)
-        # self.label = ctk.CTkLabel(master=self.frame2, text="Playlist")
-        # self.label.pack(pady=12, padx=10)
-    def printsomething(self):
-        print("hello")
-
+        
     def handleDownloads(self):
-        self.progressBar.configure(mode="undeterminate", progress_color="darkcyan")
+        self.handler.onDownloadBar()
         if(self.checkboxValue.get() == 1):
-           result = threads.AudioThread(self.entry.get())
+           result = threads.AudioThread(self.handler.entry.get())
            if(result == True):
-               self.progressBar.configure(mode="determinate", progress_color="green")
-               self.progressBar.set(100)
-               self.progressBar.stop()
-               
+                self.handler.afterDownloadBar()
             
         else:
-            result = threads.VideoThread(self.entry.get())
-            
+            result = threads.VideoThread(self.handler.entry.get())
             if(result == True):
-                self.progressBar.stop()
+                self.handler.afterDownloadBar()
+                
 
     def handleThread(self):
         buttonThread = th(target=self.handleDownloads)
         buttonThread.start()
 
-app = App("Pyload", 650, 400, "dark")
-app.setAppConfig()
-app.setTabView(MyTabView)
+app = MyTabView()
+# app.setAppConfig()
+# app.setTabView(MyTabView)
 app.mainloop()
--- Repositories/tableandwidget_handler.py
+++ Repositories/tableandwidget_handler.py
@@ -5,54 +5,51 @@ from Services.app_services import Threads
 threads = Threads()
 class TabAndWidgetHandler(CTkTabview):
     def __init__(self, master, **kwargs):
-        self.checkboxValue = 1
+        self.master = master
+        self.UIFont = CTkFont(family="Segoe UI", weight="bold")
         super().__init__(master, **kwargs)
     
 
     @abstractmethod
-    def tabConfig(self, *args, **kwargs):
-        self.configure(*args, **kwargs)
-        
-    @abstractmethod
-    def addTab(self, quantity, tabName):
-        self.tabNameList = []
-        for tab in range(quantity):
-            self.add(f"{tabName[tab]}")
-            self.tabNameList.append(tabName[tab])
-        return self.tabNameList
-
-    @abstractmethod
-    def setFrame(self, tabList, frameQuantity, tabName):
-        frameList = []
-        for frame in range(frameQuantity):
-            self.frame = CTkFrame(master=self.tab(tabList[frame]))
-            self.frame.pack(pady=30, padx=60, fill="both", expand=True)
-            frameList.append(self.frame)
-        return frameList
+    def setTabAndFrame(self, tabName):
+        self.add(tabName)
+        self.frame = CTkFrame(master=self.tab(tabName))
+        self.frame.pack(padx=20, pady=20, fill="both", expand=True)
             
 
     @abstractmethod
-    def setLabel(self, frame, text):
-        self.label = CTkLabel(master=frame, text=text)
+    def setLabel(self, text):
+        self.label = CTkLabel(master=self.frame, text=text)
         self.label.pack(pady=12, padx=10)
 
     @abstractmethod
-    def setEntry(self, frame, placeholder_text):
-        self.entry = CTkEntry(master=frame, placeholder_text=placeholder_text)
+    def setEntry(self, *args, **kwargs):
+        self.entry = CTkEntry(master=self.frame, *args, **kwargs)
         self.entry.pack(pady=12, padx=10)
+
     @abstractmethod
-    def setCheckBox(self, frame, text, variable, onvalue=1, offvalue=0):
-        self.checkbox = CTkCheckBox(master=frame, text=text, variable=variable, onvalue=onvalue, offvalue=offvalue)
+    def setCheckBox(self,text, variable):
+        self.checkbox = CTkCheckBox(master=self.frame, text=text, variable=variable, onvalue=1, offvalue=0)
         self.checkbox.pack(pady=12, padx=10)
 
     @abstractmethod
-    def button(self, frame, hover_color, font, text, *args, **kwargs):
-        self.button = CTkButton(master=frame, hover_color=hover_color, font=font, text=text, *args, **kwargs)
+    def setButton(self, hover_color, text, *args, **kwargs):
+        self.button = CTkButton(master=self.frame, hover_color=hover_color, font=self.UIFont, text=text, *args, **kwargs)
         self.button.pack(pady=12, padx=10)
 
     @abstractmethod
-    def setProgressBar(self, frame, orientation, progress_color, mode):
-        self.progressBar = CTkProgressBar(master=frame, orientation=orientation, progress_color=progress_color, mode=mode)
-        return self.progressBar
-    
+    def setProgressBar(self, orientation, progress_color, mode):
+        self.progressBar = CTkProgressBar(master=self.frame, orientation=orientation, progress_color=progress_color, mode=mode)
+        self.progressBar.pack(pady=12, padx=10)
+
+    @abstractmethod
+    def onDownloadBar(self):
+        self.progressBar.configure(mode="undeterminate", progress_color="darkcyan")
+
+    @abstractmethod
+    def afterDownloadBar(self):
+        self.progressBar.configure(mode="determinate", progress_color="green")
+        self.progressBar.set(100)
+        self.progressBar.stop()
+
     
